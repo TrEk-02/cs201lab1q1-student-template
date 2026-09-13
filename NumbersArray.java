d5bf2c22b0173c33cba8827cee6877ac36a886b9
TrEk-02 <gh.tung.2025@smu.edu.sg>
2026-09-13 17:52:09 +0800

    solved q1

--- NumbersArray.java
+++ NumbersArray.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class NumbersArray {
+
     public static void main(String[] args) {
         Integer[] input = {1,2,3,4,1,5};
 
@@ -20,6 +21,55 @@ public class NumbersArray {
     }
 
     // Write your methods here
+    public static int findMax(Integer[] arr){
+        int result = arr[0];
+
+        for (int i = 1; i < arr.length; i++){
+            if (result < arr[i]){
+                result = arr[i]; 
+            } 
+        }
+
+        return result;
+    }
+
+    public static int[] findDuplicates(Integer[] arr){
+
+        Set<Integer> seen = new HashSet<>();
+
+        int[] result = Arrays.stream(arr)
+                             .filter(n -> !seen.add(n))
+                             .mapToInt(Integer::intValue)
+                             .toArray();        
+
+        return result;
+    }
+
+    public static int[] findUnique(Integer[] arr){
+        
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++){
+            boolean isUnique = true; 
+
+            for (int j = 0; j < arr.length; j++){
+
+                if (i != j && arr[i].equals(arr[j])){
+                    isUnique = false;
+                    break;
+                }
+            }
+
+            if (isUnique){
+                int e = arr[i];
+                result.add(e);
+            }
+            
+        }
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
+
+    }
     
 }
 
